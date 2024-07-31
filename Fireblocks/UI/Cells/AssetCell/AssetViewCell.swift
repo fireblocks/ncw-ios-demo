62e03336591b900c1338daabc2b52e13fc177a9d
dudisg-fb <141024263+dudisg-fb@users.noreply.github.com>
2024-07-31 11:33:12 +0300

    Release/2.7.0 (#16)

--- Fireblocks/UI/Cells/AssetCell/AssetViewCell.swift
+++ Fireblocks/UI/Cells/AssetCell/AssetViewCell.swift
@@ -7,15 +7,9 @@
 
 import UIKit
 
-class AssetViewCell: UITableViewCell {
+class AssetViewCell: AddAssetViewCell {
 
 //MARK: - PROPERTIES
-    @IBOutlet weak var imageBackground: UIView!
-    @IBOutlet weak var assetImage: UIImageView!
-    @IBOutlet weak var assetName: UILabel!
-    @IBOutlet weak var assetAbbreviation: UILabel!
-    @IBOutlet weak var assetBlockchainBadgeBackground: UIView!
-    @IBOutlet weak var assetBlockchainBadge: UILabel!
     @IBOutlet weak var assetTokenAmount: UILabel!
     @IBOutlet weak var assetValue: UILabel!
     
@@ -23,7 +17,7 @@ class AssetViewCell: UITableViewCell {
 //MARK: - LIFECYCLE Functions
     override func awakeFromNib() {
         super.awakeFromNib()
-        ConfigCellView()
+        configCellView()
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -32,32 +26,19 @@ class AssetViewCell: UITableViewCell {
     
     override func prepareForReuse() {
         super.prepareForReuse()
-        assetImage.image = nil
-        assetName.text = ""
-        assetAbbreviation.text = ""
-        assetBlockchainBadge.text = ""
-        assetBlockchainBadgeBackground.isHidden = true
         assetValue.text = "$0"
         assetTokenAmount.text = "0"
     }
     
 //MARK: - FUNCTIONS
-    private func ConfigCellView(){
+    override func configCellView(){
         imageBackground.layer.cornerRadius = 9
         assetBlockchainBadgeBackground.layer.cornerRadius = assetBlockchainBadgeBackground.frame.height / 2
     }
     
     func configCellWith(asset: Asset, isBlockchainHidden: Bool = false) {
-        if let iconURL = asset.iconUrl {
-            assetImage.sd_setImage(with: URL(string: iconURL), placeholderImage: asset.image)
-        } else {
-            assetImage.image = asset.image
-        }
-        assetName.text = asset.name
-        assetAbbreviation.text = asset.symbol
-        assetBlockchainBadge.text = asset.blockchain
-        assetBlockchainBadge.isHidden = isBlockchainHidden
-        assetBlockchainBadgeBackground.isHidden = isBlockchainHidden
+        configAssetView(asset: asset, isBlockchainHidden: isBlockchainHidden)
+
         if let price = asset.price {
             assetValue.text = "$\(price.formatFractions(fractionDigits: 2))"
         }
@@ -66,4 +47,5 @@ class AssetViewCell: UITableViewCell {
         }
 
     }
+    
 }
